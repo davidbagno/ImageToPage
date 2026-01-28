f78c694f00ce650b04a7d87e7b362422319c07d4
David Bagno <dbagno@mac.com>
2026-01-28 09:55:45 -0500

    AzureVisionService UI implemented

--- AzureLogin/Services/AzureVisionService.cs
+++ AzureLogin/Services/AzureVisionService.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Azure;
 using Azure.AI.Vision.ImageAnalysis;
 using Microsoft.Extensions.Logging;
